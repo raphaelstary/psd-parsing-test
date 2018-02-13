2dc665c40e3943b1b854769d3760042343eccf01
raphael <raphael.stary@gmail.com>
2018-02-13 15:55:18 -0800

    read out the basic info like dimensions out of a png file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const stream = fs.createReadStream('test.png', {
+const headerStream = fs.createReadStream('test.png', {
     start: 0,
     end: 7
 });
@@ -16,15 +16,44 @@ const PNG_MAGIC = new Uint8Array([
     10
 ]);
 
-const buffer = new Uint8Array(8);
-
-let i = 0;
-stream.on('data', (chunk) => {
-    buffer.set(chunk, i);
-    i += chunk.length;
+const headerBuffer = new Uint8Array(8);
+headerStream.on('data', (chunk) => {
+    headerBuffer.set(chunk);
 });
 
-stream.on('end', () => {
-    const isPNG = buffer.every((value, index) => value == PNG_MAGIC[index]);
-    console.log(isPNG);
+headerStream.on('end', () => {
+    const isPNG = headerBuffer.every((value, index) => value == PNG_MAGIC[index]);
+
+    if (isPNG) {
+        parseBody();
+    } else {
+        throw 'file is no PNG';
+    }
 });
+
+function parseBody() {
+    const stream = fs.createReadStream('test.png');
+
+    let firstPass = true;
+
+    stream.on('data', (chunk) => {
+        if (firstPass) {
+            firstPass = false;
+
+            console.log('length: ' + chunk.readUInt32BE(8));
+            console.log('length: ' + chunk.toString('utf8', 8 + 4, 8 + 4 + 4));
+
+            console.log('width: ' + chunk.readUInt32BE(8 + 4 + 4));
+            console.log('height: ' + chunk.readUInt32BE(8 + 4 + 4 + 4));
+            console.log('bit depth: ' + chunk.readUInt8(8 + 4 + 4 + 4 + 4));
+            console.log('color: ' + chunk.readUInt8(8 + 4 + 4 + 4 + 4 + 1));
+            console.log('compression: ' + chunk.readUInt8(8 + 4 + 4 + 4 + 4 + 1 + 1));
+            console.log('filter: ' + chunk.readUInt8(8 + 4 + 4 + 4 + 4 + 1 + 1 + 1));
+            console.log('interlace: ' + chunk.readUInt8(8 + 4 + 4 + 4 + 4 + 1 + 1 + 1 + 1));
+
+            console.log('CRC: ' + chunk.readUInt32BE(29));
+        }
+    });
+
+    stream.on('end', () => console.log('\n' + 'hope this info helps :)'));
+}
